24465d9c455834503389658850cda7a1cab4a57f
Alice Ribeiro Paz da Rosa <alicerosamat@gmail.com>
2024-03-27 10:53:59 -0300

    update

--- src/pages/Input/index.jsx
+++ src/pages/Input/index.jsx
@@ -1,9 +1,10 @@
 import { forwardRef } from 'react'
 import style from '../../styles/style.module.scss'
 
-export const Input = forwardRef (({label, error, ...rest}, ref) => {
+export const Input = forwardRef (({children, label, error, ...rest}, ref) => {
     return (
         <div className={style.formItem}>
+            {children}
             <label >
                 {label}
                 <p>
